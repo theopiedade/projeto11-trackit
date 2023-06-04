dcc92b143b3cd6a0330db1b4383a75b2dda3ecf2
Theo <theo.piedade@gmail.com>
2023-06-04 01:32:03 -0300

    Starting Context application

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import Heading from '../src/Heading';
-import Section from '../src/Section';
-import styled from "styled-components"
 import { useState } from 'react'
-import HomePage from "./pages/Homepage/HomePage"
-import Cadastro from "./pages/Cadastro/Cadastro"
-import Hoje from "./pages/Hoje/Hoje"
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import axios from 'axios';
+import Context from "./Context"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from "./pages/Homepage/HomePage";
+import Cadastro from "./pages/Cadastro/Cadastro";
+import Hoje from "./pages/Hoje/Hoje";
+import Habitos from "./pages/Habitos/Habitos";
+import Historico from "./pages/Historico/Historico";
+
 
 export default function App() {
+
+  const [userLogged, setUserLogged] = useState("");
+  const [daylyProgress, setDaylyProgress] = useState("");
 
   axios.defaults.headers.common['Authorization'] = 'gKRxLmMLBYEInirlQ2e3FNVt';
 
   return (
     <>
-      <BrowserRouter>
+    <Context.Provider user={userLogged} progress={daylyProgress}>
+          <BrowserRouter>
 
-        <Routes>
+                <Routes>
 
-        <Route path='/' element={  <HomePage /> } />
-        <Route path='/cadastro' element={  <Cadastro/> } />
-        <Route path='/hoje' element={  <Hoje/> } />
+                  <Route path='/' element={  <HomePage /> } />
+                  <Route path='/cadastro' element={  <Cadastro/> } />
+                  <Route path='/hoje' element={  <Hoje/> } />
+                  <Route path='/habitos' element={  <Habitos/> } />
+                  <Route path='/habitos' element={  <Historico/> } />
 
+                </Routes>
 
-        </Routes>
-
-      </BrowserRouter>
+          </BrowserRouter>
+    </Context.Provider>
 
     </>
   )
